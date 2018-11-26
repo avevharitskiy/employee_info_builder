7194f65c2b4e64a2770952b6313a6d6e443332a0
egor43 <egor43.rus@mail.ru>
2018-11-26 23:58:06 +0300

    Добавлен модуль вызова к бл онлайна

---
+++ server/bl_invoke.py
@@ -0,0 +1,16 @@
+"""
+    Модуль работы с внешними вызовами бизнес-логики online.sbis.ru
+"""
+
+import requests
+
+def get_contacts(query_str, sid, pid):
+    """
+        Возвращает список контактов по строке строке запроса.
+        :param query_str: строка запроса
+        :param sid: идентификатор сессии
+        :param pid: идентификатор пользователя
+        :return: список контактов
+    """
+    return None
+
