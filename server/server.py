7980b78e988c8cf147d06c25445eb9a377933d7a
egor43 <egor43.rus@mail.ru>
2018-11-26 23:57:50 +0300

    Добавлены комментарии к модулю

--- server/server.py
+++ server/server.py
@@ -1,3 +1,8 @@
+"""
+    Модуль Flask. Предназначен для работы с запросами.
+    Предоставляет API проекта.
+"""
+
 try:
     from flask import Flask
 except ImportError:
@@ -7,6 +12,10 @@ flask_server = Flask(__name__)
 
 @flask_server.route('/', methods=['GET', 'POST'])
 def about():
+    """
+        Возвращает информацию о сервере
+    :return: информация о сервере
+    """
     return "This is Flask server."
 
 if __name__ == '__main__':
