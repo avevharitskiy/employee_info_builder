ebbb4250f21578d03e023bbe804fc421026c5888
egor43 <egor43.rus@mail.ru>
2018-11-27 02:28:29 +0300

    добавлен пример вызова метода получения данных с БЛ

--- server/server.py
+++ server/server.py
@@ -6,6 +6,7 @@
 from flask import Flask
 
 import json
+import bl_invoke
 
 flask_server = Flask(__name__)
 
@@ -15,7 +16,14 @@ def about():
         Возвращает информацию о сервере
         :return: информация о сервере
     """
+    sid = '00000003-00066e3e-00ba-b977e62f351fda3b'
+    pid = 'fcdb06d6-1b50-4aee-8336-b13d39e19e65'
+    query_str = 'Демо'
+    contragent = '-2'
+    bl_invoke.get_contacts(sid, pid, query_str, contragent)
     return "This is Flask server."
+
+
 
 if __name__ == '__main__':
     flask_server.run()
